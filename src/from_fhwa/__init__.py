ee190bf1675efe580b8ce020a990992d10544a7b
Schramm9 <schrammcj@gmail.com>
2026-03-13 14:36:03 -0400

    old package removed

--- src/from_fhwa/__init__.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Dec 24 15:12:46 2025
-
-@author: Chris
-"""
--- src/from_fhwa/discovery/__init__.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Dec 24 15:12:46 2025
-
-@author: Chris
-"""
-
-from .utils import infer_dataset_type
